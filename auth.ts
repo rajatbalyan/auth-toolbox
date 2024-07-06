68aa146ee4f4516ea85d156ec418eda600107f0a
Rajat Balyan <balyanrajat1812@gmail.com>
2024-07-06 14:39:59 +0530

    OAuth Complete

--- auth.ts
+++ auth.ts
@@ -1,16 +1,49 @@
-import NextAuth from "next-auth"
-import { PrismaAdapter } from "@auth/prisma-adapter"
+import NextAuth from "next-auth";
+import { UserRole } from "@prisma/client";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 
-import { db } from "@/lib/db"
-import authConfig from "@/auth.config"
- 
-export const { 
-  handlers, 
-  auth, 
-  signIn, 
-  signOut 
-} = NextAuth({
+import { db } from "@/lib/db";
+import authConfig from "@/auth.config";
+import { getUserById } from "@/data/user";
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  pages: {
+    signIn: "/auth/login",
+    error: "/auth/error",
+  },
+  events: {
+    async linkAccount({ user }) {
+      await db.user.update({
+        where: { id: user.id },
+        data: { emailVerified: new Date() }
+      })
+    }
+  },
+  callbacks: {
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+
+      return session;
+    },
+    async jwt({ token }) {
+      if(!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+
+      if(!existingUser) return token;
+
+      token.role = existingUser.role;
+
+      return token;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
-})
+});
--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -4,6 +4,7 @@ import * as z from "zod"
 import { useState, useTransition } from "react"
 import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
+import { useSearchParams } from "next/navigation"
 
 import { LoginSchema } from "@/schemas"
 import {
@@ -23,6 +24,11 @@ import { FormSuccess } from "@/components/form-success"
 import { login } from "@/actions/login"
 
 export const LoginForm = () => {
+    const searchParams = useSearchParams();
+    const urlError = searchParams.get("error") === "OAuthAccountNotLinked"
+    ? "Email already in use with different provider!"
+    : ""
+
     const [error, setError] = useState<string | undefined>("");
     const [success, setSuccess] = useState<string | undefined>("");
     const [isPending, startTransition] = useTransition();
@@ -43,7 +49,8 @@ export const LoginForm = () => {
             login(values)
             .then((data) => {
                 setError(data?.error);
-                setSuccess(data?.success);
+                // TODO: Add when we add 2FA
+                // setSuccess(data?.success);
             });
         });
     }
@@ -98,7 +105,7 @@ export const LoginForm = () => {
                             )}
                         />
                     </div>
-                    <FormError message={error} />
+                    <FormError message={error || urlError} />
                     <FormSuccess message={success} />
                     <Button
                         disabled={isPending}
